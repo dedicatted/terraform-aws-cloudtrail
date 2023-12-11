df9589931ec3865b9d05f0e0cce7842646c8477a
sunrooff <dima.kononenko@gmail.com>
2023-12-11 20:03:16 +0200

    IDT-20, test2

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Terraform Module: terraform-aws-cloudtrail
+# This module facilitates the enabling CloudTrail with creation of encrypted S3 bucket for logs.
 
 ## Overview
 The `terraform-aws-cloudtrail` module enables CloudTrail to capture all compatible management events in region. 
+
+For capturing events from services like IAM, include_global_service_events must be enabled.
 
 ## Usage
 ```hcl
--- variables.tf
+++ variables.tf
@@ -3,3 +3,33 @@ variable "region" {
   default     = "us-east-1"
   description = "AWS region to deploy."
 }
+
+variable "cloudtrail_name" {
+  type        = string
+  default     = "cloudtrail"
+  description = "Name for the Cloudtrail."
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  default     = "s3-cloudtrail-logs"
+  description = "Name of the S3 bucket designated for publishing log files."
+}
+
+variable "include_global_service_events" {
+  type        = bool
+  default     = false
+  description = "For capturing events from services like IAM, include_global_service_events must be enabled."
+}
+
+variable "force_destroy" {
+  type        = bool
+  default     = true
+  description = "Allow deletion of non-empty bucket."
+}
+
+variable "s3_key_prefix" {
+  type        = string
+  default     = "cloudtrail"
+  description = "S3 key prefix for CloudTrail logs"
+}
