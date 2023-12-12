323534542ef27cf03fbad9eb9e2f37bb10fc3862
sunrooff <dima.kononenko@gmail.com>
2023-12-12 11:41:03 +0200

    IDT-20, test4

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -6,14 +6,12 @@ resource "aws_cloudtrail" "cloudtrail" {
   s3_bucket_name                = aws_s3_bucket.example.id
   s3_key_prefix                 = var.s3_key_prefix
   include_global_service_events = var.include_global_service_events
-
-
-  # enable_log_file_validation    = var.enable_log_file_validation
-  # is_multi_region_trail         = var.is_multi_region_trail
-  # cloud_watch_logs_role_arn     = var.cloud_watch_logs_role_arn
-  # cloud_watch_logs_group_arn    = var.cloud_watch_logs_group_arn
-  # kms_key_id                    = join("", aws_kms_key.cloudtrail[*].arn)
-  # is_organization_trail         = var.is_organization_trail
+  enable_log_file_validation    = var.enable_log_file_validation
+  is_multi_region_trail         = var.is_multi_region_trail
+  cloud_watch_logs_role_arn     = var.cloud_watch_logs_role_arn
+  cloud_watch_logs_group_arn    = var.cloud_watch_logs_group_arn
+  kms_key_id                    = join("", aws_kms_key.cloudtrail[*].arn)
+  is_organization_trail         = var.is_organization_trail
   # tags                          = module.labels.tags
   # sns_topic_name                = var.sns_topic_name
 
@@ -29,4 +27,201 @@ resource "aws_s3_bucket" "example" {
 resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
   policy = data.aws_iam_policy_document.example.json
+}
+
+
+
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.example.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
+
+data "aws_iam_policy_document" "cloudtrail_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+
+## ==============================================================================================
+# This role is used by CloudTrail to send logs to CloudWatch.
+## ==============================================================================================
+
+resource "aws_iam_role" "cloudtrail_cloudwatch_role" {
+  count              = var.enable_cloudwatch && var.enabled_cloudtrail ? 1 : 0
+  name               = var.iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.cloudtrail_assume_role.json
+}
+resource "aws_cloudwatch_log_group" "cloudtrail" {
+  count             = var.enable_cloudwatch && var.enabled_cloudtrail ? 1 : 0
+  name              = var.cloudwatch_log_group_name
+  retention_in_days = var.log_retention_days
+  kms_key_id        = join("", aws_kms_key.cloudtrail[*].arn)
+}
+
+data "aws_iam_policy_document" "cloudtrail_cloudwatch_logs" {
+  statement {
+    sid = "WriteCloudWatchLogs"
+
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    #tfsec:ignore:aws-iam-no-policy-wildcards
+    resources = ["arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:cloudwatch-log-group:*"]
+  }
+}
+resource "aws_iam_policy" "cloudtrail_cloudwatch_logs" {
+  count  = var.enable_cloudwatch && var.enabled_cloudtrail ? 1 : 0
+  name   = "cloudtrail-cloudwatch-logs-policy"
+  policy = data.aws_iam_policy_document.cloudtrail_cloudwatch_logs.json
+}
+resource "aws_iam_policy_attachment" "main" {
+  count      = var.enable_cloudwatch && var.enabled_cloudtrail ? 1 : 0
+  name       = "cloudtrail-cloudwatch-logs-policy-attachment"
+  policy_arn = aws_iam_policy.cloudtrail_cloudwatch_logs[0].arn
+  roles      = [aws_iam_role.cloudtrail_cloudwatch_role[0].name]
+}
+
+## ==============================================================================================
+## Supports only for single account cloudtrail.
+## ==============================================================================================
+
+resource "aws_kms_key" "cloudtrail" {
+  count                   = var.kms_enabled && var.enabled_cloudtrail ? 1 : 0
+  description             = "A KMS key used to encrypt CloudTrail log files stored in S3."
+  deletion_window_in_days = var.key_deletion_window_in_days
+  enable_key_rotation     = "true"
+  policy                  = data.aws_iam_policy_document.kms.json
+  tags                    = module.labels.tags
+}
+
+data "aws_iam_policy_document" "kms" {
+  version = "2012-10-17"
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "Allow CloudTrail to encrypt logs"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions   = ["kms:GenerateDataKey*"]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+    }
+  }
+
+  statement {
+    sid    = "Allow CloudTrail to describe key"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions   = ["kms:DescribeKey"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow principals in the account to decrypt log files"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:ReEncryptFrom"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+    }
+  }
+
+  statement {
+    sid    = "Allow alias creation during setup"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["kms:CreateAlias"]
+    resources = ["*"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -39,3 +39,77 @@ variable "enabled_cloudtrail" {
   default     = true
   description = "If true, deploy the resources for the module."
 }
+
+variable "enable_log_file_validation" {
+  type        = bool
+  default     = true
+  description = "Specifies whether log file integrity validation is enabled. Creates signed digest for validated contents of logs."
+}
+
+variable "is_multi_region_trail" {
+  type        = bool
+  default     = false
+  description = "Specifies whether the trail is created in the current region or in all regions"
+}
+
+variable "cloud_watch_logs_role_arn" {
+  type        = string
+  default     = ""
+  description = "Specifies the role for the CloudWatch Logs endpoint to assume to write to a user’s log group."
+  sensitive   = true
+}
+
+variable "cloud_watch_logs_group_arn" {
+  type        = string
+  default     = ""
+  description = "Specifies a log group name using an Amazon Resource Name (ARN), that represents the log group to which CloudTrail logs will be delivered."
+  sensitive   = true
+}
+
+variable "is_organization_trail" {
+  type        = bool
+  default     = false
+  description = "The trail is an AWS Organizations trail."
+}
+
+variable "sns_topic_name" {
+  type        = string
+  default     = null
+  description = "Specifies the name of the Amazon SNS topic defined for notification of log file delivery."
+}
+
+variable "enable_cloudwatch" {
+  type        = bool
+  default     = true
+  description = "If true, deploy the resources for cloudwatch in the module."
+}
+
+variable "iam_role_name" {
+  type        = string
+  default     = "cloudtrail-cloudwatch-logs-role"
+  description = "Name for the CloudTrail IAM role"
+}
+
+variable "cloudwatch_log_group_name" {
+  type        = string
+  default     = "cloudtrail-events"
+  description = "The name of the CloudWatch Log Group that receives CloudTrail events."
+}
+
+variable "log_retention_days" {
+  type        = string
+  default     = 90
+  description = "Number of days to keep AWS logs around in specific log group."
+}
+
+variable "kms_enabled" {
+  type        = bool
+  default     = false
+  description = "If true, deploy the resources for kms in the module. Note: Supports in only single cloudtrail management."
+}
+
+variable "key_deletion_window_in_days" {
+  type        = string
+  default     = 30
+  description = "Duration in days after which the key is deleted after destruction of the resource, must be 7-30 days.  Default 30 days."
+}
